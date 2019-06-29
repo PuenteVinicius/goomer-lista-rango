55ad8a1ae7cb2dfe604364d4d78bf4c58b82334a
Vinicius Puente <viniciuspuente2@gmail.com>
2019-06-29 19:21:08 -0300

    - add serachRestaurant component

---
+++ src/containers/search-restaurants/searchRestaurants.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux'
+
+import { bindActionCreators } from 'redux'
+
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import { filterRestaurants } from './searchRestaurantsActions'
+
+class SearchRestaurant extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: props.initialValue}
+
+    this.handleChange = this.handleChange.bind(this)
+    console.log(this.props);
+  }
+
+  handleChange(event) {
+
+    this.setState({ value: event.target.value })
+  }
+  
+  render() {
+    return (
+    <Paper className="search-restaurant" >
+      <InputBase onChange={this.props.filterRestaurants} className="search-restaurant__input" />
+      <IconButton className="search-restaurant__button" aria-label="Search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+      value: state.searchRestaurants.value
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ filterRestaurants }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchRestaurant);
