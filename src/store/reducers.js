648aaff2e127088ff0638477cb786ef44deaa31e
Vinicius Puente <viniciuspuente2@gmail.com>
2019-06-29 01:35:50 -0300

    - add reducers for cardRestaurant

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import searchReducer from '../components/search/searchReducer';
-
+import cardRestaurantReducer from '../components/card-restaurant/card-restaurantReducer';
 
 const reducers = combineReducers({
   form: formReducer,
-  search: searchReducer
+  search: searchReducer,
+  cardRestaurant:cardRestaurantReducer
 })
 
 export default reducers
