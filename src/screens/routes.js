da09414f33e22a5946a678219ca53623417d4447
Vinicius Puente <viniciuspuente2@gmail.com>
2019-07-02 03:33:04 -0300

    - add localStorage to REdux

--- src/screens/routes.js
+++ src/screens/routes.js
@@ -13,6 +13,5 @@ export default props => (
       <Route exact path='/' component={ScreenHome} />
       <Route exact path='/restaurant/:cardId' component={ScreenRestaurant} />
     </main>
-    <Redirect from='*' to='/' />  
   </Router>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -4,8 +4,33 @@ import multi from 'redux-multi';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem('state')
+    if (serializedState === null) return undefined
+    return JSON.parse(serializedState)
+  } catch(e) {
+    console.log(e)
+    return undefined
+  }
+}
+
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
 && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools);
+
+const persistedState = loadFromLocalStorage()
+
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, persistedState, devTools);
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export default store;
